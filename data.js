12624893c670bedc0ef6261fcea75c189709a38f
suppsialk-cloud <suppsialk@gmail.com>
2025-12-17 14:50:37 +0330

    sialkceramfarsi

---
+++ data.js
@@ -0,0 +1,480 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "پذیرایی",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 0.01679786939416772,
+        "pitch": 0.05082111936258826,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0059313260243368,
+          "pitch": 0.23522428077491675,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": 1.7695630759404066,
+          "pitch": 0.15261906054079333,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": 0.5530585834122412,
+          "pitch": 0.29358937096303706,
+          "rotation": 0,
+          "target": "5--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.36392731927158195,
+          "pitch": 0.5903853016354059,
+          "title": "ولورا",
+          "text": "<div>80*80 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        },
+        {
+          "yaw": -2.921671726520117,
+          "pitch": -0.042205587333675965,
+          "title": "نالینا",
+          "text": "<div>60*120 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        },
+        {
+          "yaw": 0.0750680942269053,
+          "pitch": 0.1730876229647187,
+          "title": "شایلین",
+          "text": "<div>60*120 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        }
+      ]
+    },
+    {
+      "id": "1--",
+      "name": "آشپزخانه ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.877938655748091,
+          "pitch": 0.24384912395046499,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -2.0755049547647744,
+          "pitch": 0.2642965067001608,
+          "rotation": 0,
+          "target": "5--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.20884265343831032,
+          "pitch": 0.33370241016137925,
+          "title": "نالینا",
+          "text": "<div>60*120 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        },
+        {
+          "yaw": -1.4435541009129889,
+          "pitch": 0.8289695560501293,
+          "title": "ولورا",
+          "text": "<div>80*80 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        },
+        {
+          "yaw": -2.81554554109635,
+          "pitch": 0.6362833140429167,
+          "title": "نالینا",
+          "text": "<div>60*120 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        }
+      ]
+    },
+    {
+      "id": "2--",
+      "name": "پیش ورودی",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0688057391004815,
+          "pitch": 0.23207746405113383,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.9647123730549492,
+          "pitch": 0.46405247573750863,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 0.1822627346839596,
+          "pitch": 0.34970471611148213,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3305400129351845,
+          "pitch": 0.6134967359874128,
+          "title": "شایلین",
+          "text": "<div>60*120 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        },
+        {
+          "yaw": -2.3538751535540925,
+          "pitch": 0.6204242993289331,
+          "title": "ولورا",
+          "text": "<div>80*80 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "حمام",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0713508391324122,
+          "pitch": 0.4928199466374039,
+          "rotation": 0,
+          "target": "5--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5663724564027532,
+          "pitch": -0.056141274006876074,
+          "title": "سرتینا روشن",
+          "text": "<div>60*120 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        },
+        {
+          "yaw": -1.026865408624877,
+          "pitch": -0.23735203962192664,
+          "title": "سرتینا تیره",
+          "text": "<div>60*120 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        },
+        {
+          "yaw": 1.5243246583736534,
+          "pitch": 0.04511463278166339,
+          "title": "قطعات پازلی کد 01",
+          "text": "17 cm² : مساحت"
+        },
+        {
+          "yaw": 0.06639809268434504,
+          "pitch": 0.4757930099869725,
+          "title": "روشویی سرامیکی کد 01",
+          "text": "100*47.5*16 cm : ابعاد<div>50 kg : وزن&nbsp;</div>"
+        },
+        {
+          "yaw": -0.8992048052196786,
+          "pitch": 0.8551222305166242,
+          "title": "Velora",
+          "text": "80*80 cm<br><div><div>Nano Polished</div><div>Porcelain Tile</div><div>Random Faces</div></div>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "توالت",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9776830085167667,
+          "pitch": 0.6033897031448099,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1832135772202168,
+          "pitch": 0.7015566495111152,
+          "title": "روشویی سرامیکی کد 02",
+          "text": "100*50*17.5 cm : ابعاد<div>50 kg : وزن&nbsp;</div>"
+        },
+        {
+          "yaw": -0.11832768643243696,
+          "pitch": -0.05006565622528569,
+          "title": "المورا",
+          "text": "<div>60*120 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        },
+        {
+          "yaw": -2.3907977055251877,
+          "pitch": 1.0244684437387122,
+          "title": "ولورا",
+          "text": "<div><div>80*80 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div></div>"
+        }
+      ]
+    },
+    {
+      "id": "5--",
+      "name": "اتاق خواب",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6424692232126876,
+          "pitch": 0.36078970418017775,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.8477707654334008,
+          "pitch": 0.2834027039813538,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.44646803905575894,
+          "pitch": -0.03469155681937153,
+          "title": "کلرادو",
+          "text": "<div>60*120 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        },
+        {
+          "yaw": 0.9588234012289156,
+          "pitch": 0.67468482031917,
+          "title": "ولورا",
+          "text": "<div>80*80 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        },
+        {
+          "yaw": -0.44361488311759345,
+          "pitch": -0.034082664813466224,
+          "title": "قطعات پازلی کد 07",
+          "text": "15.5 cm² : مساحت"
+        }
+      ]
+    },
+    {
+      "id": "6-",
+      "name": "لابی",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9883478548700095,
+          "pitch": 0.2961668948847276,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.0939954609863562,
+          "pitch": 0.5132088403734318,
+          "title": "ریبرا",
+          "text": "<div>80*80 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        },
+        {
+          "yaw": 2.7301930134493837,
+          "pitch": 0.010565553863347787,
+          "title": "ریواز",
+          "text": "<div>60*120 cm<br>مات</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        },
+        {
+          "yaw": 1.4424034359838398,
+          "pitch": 0.022498844593009437,
+          "title": "بلک رز",
+          "text": "<div>60*120 cm<br>نانو پولیش</div><div>پرسلان</div><div>فیس های تصادفی</div>"
+        }
+      ]
+    }
+  ],
+  "name": "sialk ceram farsi",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
